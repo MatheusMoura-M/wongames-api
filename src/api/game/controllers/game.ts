76279a80fd641f147fe5cb68812592f72730f7b4
Matheus <matheus150101miranda@gmail.com>
2024-11-03 12:17:20 -0300

    feat: structure of the create game and others types

---
+++ src/api/game/controllers/game.ts
@@ -0,0 +1,17 @@
+/**
+ * game controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("RODANDO NO SERVIDOR");
+
+      await strapi.service("api::game.game").populate(ctx.query);
+      ctx.send("FINALIZADO NO CLIENT");
+    },
+  })
+);

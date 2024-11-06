08cf02283f1e395a8ca23e6857e1278920b3900d
Matheus <matheus150101miranda@gmail.com>
2024-11-06 08:55:02 -0300

    feat: add custom query params

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -8,10 +8,15 @@ export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
     async populate(ctx) {
-      console.log("RODANDO NO SERVIDOR");
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
 
-      await strapi.service("api::game.game").populate(ctx.query);
-      ctx.send("FINALIZADO NO CLIENT");
+      await strapi.service("api::game.game").populate(options);
+
+      ctx.send("Finished populating games!");
     },
   })
 );
